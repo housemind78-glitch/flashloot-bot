b17d8ca27bfd3509b5572eb35b84b117c7cce427
housemind78-glitch <housemind78@gmail.com>
2026-05-08 21:48:22 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import requests
+import random
+
+BOT_TOKEN = "8242223953:AAHwT4z3k2P4Gd5eO_t0B1CQdFu92XuUbZ4"
+CHAT_ID = "@FlashLootDeals"
+
+deals = [
+    "https://amzn.to/3OUEjEe",
+    "https://amzn.to/49pkvQb",
+    "https://amzn.to/4d8Bfw5"
+]
+
+messages = [
+    "🔥 FLASH DEAL ALERT\n⚡ Limited Time Offer\n👉 {}",
+    "💸 HOT DEAL LIVE\n🛍️ Grab Fast\n👉 {}",
+    "⚡ MEGA OFFER\n🔥 Limited Stock\n👉 {}"
+]
+
+link = random.choice(deals)
+message = random.choice(messages).format(link)
+
+url = f"https://api.telegram.org/bot{8242223953:AAHwT4z3k2P4Gd5eO_t0B1CQdFu92XuUbZ4}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
